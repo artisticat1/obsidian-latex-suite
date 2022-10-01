499a1c7f485b6c32c3bf94bd163d4223ec923ae9
artisticat <100622574+artisticat1@users.noreply.github.com>
2022-10-01 12:17:34 +0100

    fix indentation: use tab size 4

--- src/inline_math_tooltip.ts
+++ src/inline_math_tooltip.ts
@@ -4,14 +4,14 @@ import { getEquationBounds, isWithinEquation, isWithinInlineEquationState } from
 import { MarkdownRenderer } from "obsidian";
 
 export const cursorTooltipField = StateField.define<readonly Tooltip[]>({
-  create: getCursorTooltips,
+	create: getCursorTooltips,
 
-  update(tooltips, tr) {
-    if (!tr.docChanged && !tr.selection) return tooltips;
-    return getCursorTooltips(tr.state);
-  },
+	update(tooltips, tr) {
+		if (!tr.docChanged && !tr.selection) return tooltips;
+		return getCursorTooltips(tr.state);
+	},
 
-  provide: f => showTooltip.computeN([f], state => state.field(f))
+	provide: f => showTooltip.computeN([f], state => state.field(f))
 });
 
 
@@ -27,19 +27,21 @@ function getCursorTooltips(state: EditorState): readonly Tooltip[] {
 
         const eqn = state.sliceDoc(bounds.start, bounds.end);
 
-        return [{
-            pos: bounds.start,
-            above: true,
-            strictSide: true,
-            arrow: true,
-            create: () => {
-              let dom = document.createElement("div");
-              dom.className = "cm-tooltip-cursor";
-              MarkdownRenderer.renderMarkdown("$" + eqn + "$", dom, "", null);
+        return [
+			{
+				pos: bounds.start,
+				above: true,
+				strictSide: true,
+				arrow: true,
+				create: () => {
+					let dom = document.createElement("div");
+					dom.className = "cm-tooltip-cursor";
+					MarkdownRenderer.renderMarkdown("$" + eqn + "$", dom, "", null);
 
-              return { dom };
-            }
-          }];
+					return { dom };
+				}
+			}
+		];
     }
     else {
         return [];
@@ -47,22 +49,24 @@ function getCursorTooltips(state: EditorState): readonly Tooltip[] {
 }
 
 
-export const cursorTooltipBaseTheme = EditorView.baseTheme({
-    ".cm-tooltip.cm-tooltip-cursor": {
-      backgroundColor: "var(--background-primary)",
-      color: "var(--text-normal)",
-      border: "1px solid var(--background-modifier-border)",
-      padding: "2px 7px",
-      borderRadius: "6px",
-      "& .cm-tooltip-arrow:before": {
-        borderTopColor: "var(--background-modifier-border)"
-      },
-      "& .cm-tooltip-arrow:after": {
-        borderTopColor: "var(--background-primary)"
-      },
-      "& p": {
-        marginTop: "2px",
-        marginBottom: "2px"
-      }
-    }
-});
+export const cursorTooltipBaseTheme = EditorView.baseTheme(
+	{
+		".cm-tooltip.cm-tooltip-cursor": {
+			backgroundColor: "var(--background-primary)",
+			color: "var(--text-normal)",
+			border: "1px solid var(--background-modifier-border)",
+			padding: "2px 7px",
+			borderRadius: "6px",
+			"& .cm-tooltip-arrow:before": {
+				borderTopColor: "var(--background-modifier-border)"
+			},
+			"& .cm-tooltip-arrow:after": {
+				borderTopColor: "var(--background-primary)"
+			},
+			"& p": {
+				marginTop: "2px",
+				marginBottom: "2px"
+			}
+    	}
+	}
+);
