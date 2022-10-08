4bb13074ea9d21c5c46bc80bc36fbf79a0570a0c
artisticat <100622574+artisticat1@users.noreply.github.com>
2022-10-08 14:04:36 +0100

    add `partial` to symbols

--- src/snippets.ts
+++ src/snippets.ts
@@ -13,7 +13,7 @@ export interface Environment {
 
 export const SNIPPET_VARIABLES = {
     "${GREEK}": "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|pi|Pi|rho|sigma|Sigma|tau|upsilon|varphi|phi|Phi|chi|psi|Psi|omega|Omega",
-    "${SYMBOL}": "hbar|ell|nabla|infty|dots|to|leftrightarrow|mapsto|setminus|mid|cap|cup|subseteq|subset|implies|impliedby|iff|exists|equiv|square|neq|geq|leq|gg|ll|sim|simeq|approx|propto|cdot|oplus|otimes|times|star|perp|det|exp|ln|log"
+    "${SYMBOL}": "hbar|ell|nabla|infty|dots|to|leftrightarrow|mapsto|setminus|mid|cap|cup|subseteq|subset|implies|impliedby|iff|exists|equiv|square|neq|geq|leq|gg|ll|sim|simeq|approx|propto|cdot|oplus|otimes|times|star|perp|det|exp|ln|log|partial"
 };
 
 export const EXCLUSIONS:{[trigger: string]: Environment} = {
