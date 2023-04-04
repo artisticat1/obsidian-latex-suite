e016c6b7d2c3e1746528ed3d0f73e9dcf149968c
Murphy <git@murphy-in.space>
2023-04-04 21:38:14 +0200

    prettify

--- src/settings.ts
+++ src/settings.ts
@@ -392,6 +392,7 @@ export class LatexSuiteSettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					})
 			);
+
 		new Setting(containerEl)
 			.setName("Highlight matching bracket beneath cursor")
 			.setDesc(
@@ -637,6 +638,20 @@ export class LatexSuiteSettingTab extends PluginSettingTab {
 					.onChange(async (value) => {
 						this.plugin.settings.wordDelimiters = value;
 
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Remove trailing whitespaces in snippets in inline math")
+			.setDesc(
+				"Whether to remove trailing whitespaces when expanding snippets at the end of inline math blocks."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.removeSnippetWhitespace)
+					.onChange(async (value) => {
+						this.plugin.settings.removeSnippetWhitespace = value;
 						await this.plugin.saveSettings();
 					})
 			);
