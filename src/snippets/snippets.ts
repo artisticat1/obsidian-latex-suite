f7204ff0a4c1c9b05526ee206098a9d5e291e96e
Murphy <git@murphy-in.space>
2023-04-10 15:18:25 +0200

    add lnot, top, bot

--- src/snippets/snippets.ts
+++ src/snippets/snippets.ts
@@ -15,8 +15,8 @@ export const SNIPPET_VARIABLES = {
 	"${GREEK}":
 		"alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|Theta|iota|kappa|lambda|Lambda|mu|nu|xi|Xi|pi|Pi|rho|sigma|Sigma|tau|upsilon|varphi|phi|Phi|chi|psi|Psi|omega|Omega",
 	"${SYMBOL}":
-		"approx|arccos|arccot|arccsc|arcsec|arcsin|arctan|cap|cdot|circ|cong|cos|cot|csc|cup|det|dots|ell|equiv|exists|exp|forall|ge|gg|hbar|iff|impliedby|implies|infty|land|leftarrow|Leftarrow|leftrightarrow|Leftrightarrow|ll|ln|log|lor|mapsto|mid|mp|nabla|ne|not|oplus|otimes|partial|perp|pm|propto|Rightarrow|setminus|simeq|sin|square|star|subseteq|tan|times|to",
-	"${SHORT_SYMBOL}": "in|le|sim|subset",
+		"approx|arccos|arccot|arccsc|arcsec|arcsin|arctan|bot|cap|cdot|circ|cong|cos|cot|csc|cup|det|dots|ell|equiv|exists|exp|forall|ge|gg|hbar|iff|impliedby|implies|infty|land|leftarrow|Leftarrow|leftrightarrow|Leftrightarrow|ll|lnot|log|lor|mapsto|mid|mp|nabla|ne|not|oplus|otimes|partial|perp|pm|propto|Rightarrow|setminus|simeq|sin|square|star|subseteq|tan|times|top",
+	"${SHORT_SYMBOL}": "in|le|ln|sim|subset|to",
 };
 
 export const EXCLUSIONS:{[trigger: string]: Environment} = {
