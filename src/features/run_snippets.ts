c53609acf5c7c5933c0d6f268574e807a735c9b3
artisticat <100622574+artisticat1@users.noreply.github.com>
2023-08-20 16:05:27 +0100

    refactor run_snippets.ts

--- src/features/run_snippets.ts
+++ src/features/run_snippets.ts
@@ -1,10 +1,8 @@
 import { SelectionRange } from "@codemirror/state";
-import { isWithinInlineEquation } from "src/editor_helpers";
 import { queueSnippet } from "src/snippets/codemirror/snippet_queue_state_field";
 import { expandSnippets } from "src/snippets/snippet_management";
 import { ParsedSnippet, SNIPPET_VARIABLES, EXCLUSIONS } from "src/snippets/snippets";
 import { autoEnlargeBrackets } from "./auto_enlarge_brackets";
-import { Options } from "src/snippets/options";
 import { Context } from "src/snippets/context";
 import { getLatexSuiteConfigFromView } from "src/snippets/codemirror/config";
 
@@ -62,60 +60,21 @@ export const runSnippetCursor = (ctx: Context, key: string, range: SelectionRang
 		}
 
 
-		const result = checkSnippet(snippet, snippet.options, effectiveLine, range, sel);
+		const result = processSnippet(snippet, effectiveLine, range, sel);
 		if (result === null) continue;
 		const triggerPos = result.triggerPos;
 
 
 		if (snippet.options.onWordBoundary) {
 			// Check that the trigger is preceded and followed by a word delimiter
-
-			const prevChar = ctx.view.state.sliceDoc(triggerPos-1, triggerPos);
-			const nextChar = ctx.view.state.sliceDoc(to, to+1);
-
-			const wordDelimiters = settings.basicSettings.wordDelimiters.replace("\\n", "\n");
-
-
-			const prevCharIsWordDelimiter = wordDelimiters.contains(prevChar);
-			const nextCharIsWordDelimiter = wordDelimiters.contains(nextChar);
-
-			if (!(prevCharIsWordDelimiter && nextCharIsWordDelimiter)) {
-				continue;
-			}
+			if (!isOnWordBoundary(ctx, triggerPos, to, settings.basicSettings.wordDelimiters)) continue;
 		}
 
 		let replacement = result.replacement;
 
-
 		// When in inline math, remove any spaces at the end of the replacement
-		if (ctx.mode.anyMath() && settings.basicSettings.removeSnippetWhitespace) {
-			let spaceIndex = 0;
-			if (replacement.endsWith(" ")) {
-				spaceIndex = -1;
-			}
-			else {
-				const lastThreeChars = replacement.slice(-3);
-				const lastChar = lastThreeChars.slice(-1);
-
-				if (lastThreeChars.slice(0, 2) === " $" && !isNaN(parseInt(lastChar))) {
-					spaceIndex = -3;
-				}
-			}
-
-			if (spaceIndex != 0) {
-
-				const inlineMath = isWithinInlineEquation(ctx.view.state);
-
-				if (inlineMath) {
-					if (spaceIndex === -1) {
-						replacement = replacement.trimEnd();
-					}
-					else if (spaceIndex === -3){
-						replacement = replacement.slice(0, -3) + replacement.slice(-2)
-					}
-				}
-			}
-
+		if (ctx.mode.inlineMath && settings.basicSettings.removeSnippetWhitespace) {
+			replacement = trimWhitespace(replacement, ctx);
 		}
 
 		// Expand the snippet
@@ -132,7 +91,7 @@ export const runSnippetCursor = (ctx: Context, key: string, range: SelectionRang
 }
 
 
-export const checkSnippet = (snippet: ParsedSnippet, options: Options, effectiveLine: string, range:  SelectionRange, sel: string):{triggerPos: number; replacement: string} => {
+export const processSnippet = (snippet: ParsedSnippet, effectiveLine: string, range:  SelectionRange, sel: string):{triggerPos: number; replacement: string} => {
 	let triggerPos;
 	let trigger = snippet.trigger;
 	trigger = insertSnippetVariables(trigger);
@@ -156,7 +115,7 @@ export const checkSnippet = (snippet: ParsedSnippet, options: Options, effective
 		// Don't run non-visual snippets when there is a selection
 		return null;
 	}
-	else if (!(options.regex)) {
+	else if (!(snippet.options.regex)) {
 
 		// Check whether the trigger text was typed
 		if (!(effectiveLine.slice(-trigger.length) === trigger)) return null;
@@ -190,6 +149,14 @@ export const checkSnippet = (snippet: ParsedSnippet, options: Options, effective
 	return {triggerPos: triggerPos, replacement: replacement};
 }
 
+export const isOnWordBoundary = (ctx: Context, triggerPos: number, to: number, wordDelimiters: string) => {
+	const prevChar = ctx.view.state.sliceDoc(triggerPos-1, triggerPos);
+	const nextChar = ctx.view.state.sliceDoc(to, to+1);
+
+	wordDelimiters = wordDelimiters.replace("\\n", "\n");
+
+	return (wordDelimiters.contains(prevChar) && wordDelimiters.contains(nextChar));
+}
 
 export const insertSnippetVariables = (trigger: string) => {
 
@@ -198,4 +165,31 @@ export const insertSnippetVariables = (trigger: string) => {
 	}
 
 	return trigger;
+}
+
+export const trimWhitespace = (replacement: string, ctx: Context) => {
+	let spaceIndex = 0;
+
+	if (replacement.endsWith(" ")) {
+		spaceIndex = -1;
+	}
+	else {
+		const lastThreeChars = replacement.slice(-3);
+		const lastChar = lastThreeChars.slice(-1);
+
+		if (lastThreeChars.slice(0, 2) === " $" && !isNaN(parseInt(lastChar))) {
+			spaceIndex = -3;
+		}
+	}
+
+	if (spaceIndex != 0) {
+		if (spaceIndex === -1) {
+			replacement = replacement.trimEnd();
+		}
+		else if (spaceIndex === -3){
+			replacement = replacement.slice(0, -3) + replacement.slice(-2);
+		}
+	}
+
+	return replacement;
 }
