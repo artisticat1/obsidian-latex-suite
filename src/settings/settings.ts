114595f4c409fad3440e64c1322a67d0ff30c34c
artisticat <100622574+artisticat1@users.noreply.github.com>
2023-10-20 15:57:01 +0100

    add \forall to `${SYMBOL}`, add default snippets for \upsilon, \not\in, \emptyset

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -74,7 +74,7 @@ export const DEFAULT_SETTINGS: LatexSuitePluginSettings = {
 	forceMathLanguages: "math",
 	snippetVariables: `{
 	"$\{GREEK}": "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|Theta|iota|kappa|lambda|Lambda|mu|nu|omicron|xi|Xi|pi|Pi|rho|sigma|Sigma|tau|upsilon|varphi|phi|Phi|chi|psi|Psi|omega|Omega",
-	"$\{SYMBOL}": "hbar|ell|nabla|infty|dots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|equiv|square|neq|geq|leq|gg|ll|sim|simeq|approx|propto|cdot|oplus|otimes|times|star|perp|det|exp|ln|log|partial",
+	"$\{SYMBOL}": "hbar|ell|nabla|infty|dots|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|equiv|square|neq|geq|leq|gg|ll|sim|simeq|approx|propto|cdot|oplus|otimes|times|star|perp|det|exp|ln|log|partial",
 	"$\{SHORT_SYMBOL}": "to|pm|mp"
 }`
 }
