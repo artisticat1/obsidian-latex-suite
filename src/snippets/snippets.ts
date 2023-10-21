76991c4841fff2cf8e107de5782ec2d4449d3027
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-10-21 14:11:44 -0700

    feat(snippets): support regexp literal triggers in js; support regexp flags
    in js snippets files, support passing in regexp literals as the trigger.
    also, generally support regexp flags for regexp snippets (i.e. regexp literals,
    or snippets with the `r` option).
    only the following flags are allowed: `i`, `m`, `s`, `u`, `v`.
    when a snippet with a regexp literal trigger also has flags defined,
    the flags get merged together.

--- src/snippets/snippets.ts
+++ src/snippets/snippets.ts
@@ -1,11 +1,12 @@
 import { Options } from "./options";
 
 export class RawSnippet {
-	trigger: string;
+	trigger: string | RegExp;
 	replacement: string;
 	options: string;
 	priority?: number;
 	description?: string;
+	flags?: string;
 }
 
 export class ParsedSnippet {
@@ -14,9 +15,31 @@ export class ParsedSnippet {
 	options: Options;
 	priority?: number;
 	description?: string;
+	flags?: string;
 
 	constructor(raw: RawSnippet) {
-		const parsed = {...raw, options: Options.fromSource(raw.options)};
+		// normalize triggers as strings
+		const override: RawSnippet = { ...raw }
+		if (raw.trigger instanceof RegExp) {
+			override.options = `r${raw.options}`;
+			override.trigger = raw.trigger.source;
+			override.flags = raw.trigger.flags;
+		}
+		const validFlags = [
+			// "d", // doesn't affect the search
+			// "g", // doesn't affect the pattern match and is almost certainly undesired behavior
+			"i",
+			"m",
+			"s",
+			"u",
+			"v",
+			// "y", // almost certainly undesired behavior
+		];
+		const resolvedFlags = Array.from(new Set(`${override.flags ?? ""}${raw.flags ?? ""}`.split("")))
+			.filter(flag => validFlags.includes(flag))
+			.join(""); 
+
+		const parsed = {...raw, ...override, flags: resolvedFlags, options: Options.fromSource(raw.options)};
 		Object.assign(this, parsed);
 	}
 }
