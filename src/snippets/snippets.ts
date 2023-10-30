54cb71e2cd89d5ab2e18380bdc0e4fd72ab0ec0a
william duan <38791932+duanwilliam@users.noreply.github.com>
2023-10-29 17:21:27 -0700

    refactor: clean up `ParsedSnippet` constructor
    remove redundant uses of raw snippet, make state of values more explicit.

--- src/snippets/snippets.ts
+++ src/snippets/snippets.ts
@@ -18,15 +18,15 @@ export class ParsedSnippet {
 	description?: string;
 	
 	constructor(raw: RawSnippet) {
-		// normalize triggers as strings
-		const override: RawSnippet = { ...raw };
+		// normalize regex triggers
+		const override: RawSnippet = { ...raw, flags: raw.flags ?? "" };
 		if (raw.trigger instanceof RegExp) {
 			override.options = `r${raw.options}`;
 			override.trigger = raw.trigger.source;
-			override.flags = raw.trigger.flags;
+			override.flags = `${raw.trigger.flags}${override.flags}`;
 		}
 
-		// merge flags
+		// filter out invalid flags
 		const validFlags = [
 			// "d", // doesn't affect the search
 			// "g", // doesn't affect the pattern match and is almost certainly undesired behavior
@@ -37,11 +37,11 @@ export class ParsedSnippet {
 			"v",
 			// "y", // almost certainly undesired behavior
 		];
-		const resolvedFlags = Array.from(new Set(`${override.flags ?? ""}${raw.flags ?? ""}`.split("")))
+		override.flags = Array.from(new Set(`${override.flags ?? ""}`.split("")))
 			.filter(flag => validFlags.includes(flag))
 			.join(""); 
 
-		const parsed = {...raw, ...override, options: Options.fromSource(override.options), flags: resolvedFlags};
+		const parsed = {...override, options: Options.fromSource(override.options)};
 		Object.assign(this, parsed);
 	}
 }
