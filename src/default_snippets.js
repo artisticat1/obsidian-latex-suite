46ae58729eb3b68dbfd7d71975122ac078bcc299
you-n-g <you-n-g@users.noreply.github.com>
2024-01-30 07:15:54 +0800

    add overbrace shortcuts.

--- src/default_snippets.js
+++ src/default_snippets.js
@@ -113,6 +113,7 @@
 
 	// Visual operations
 	{trigger: "U", replacement: "\\underbrace{ ${VISUAL} }_{ $0 }", options: "mA"},
+	{trigger: "O", replacement: "\\overbrace{ ${VISUAL} }^{ $0 }", options: "mA"},
 	{trigger: "B", replacement: "\\underset{ $0 }{ ${VISUAL} }", options: "mA"},
 	{trigger: "C", replacement: "\\cancel{ ${VISUAL} }", options: "mA"},
 	{trigger: "K", replacement: "\\cancelto{ $0 }{ ${VISUAL} }", options: "mA"},
