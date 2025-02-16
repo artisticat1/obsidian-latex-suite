402fc6eaeba9798f202878c59da3426d8979095b
Tortoise <195262249+Testudinidae@users.noreply.github.com.>
2025-02-16 01:08:42 -0800

    feat: refactor and add multi-selection support

--- src/features/matrix_shortcuts.ts
+++ src/features/matrix_shortcuts.ts
@@ -1,8 +1,44 @@
 import { EditorView } from "@codemirror/view";
+import { EditorSelection, SelectionRange } from "@codemirror/state";
+import { Context } from "src/utils/context";
 import { setCursor } from "src/utils/editor_utils";
 import { getLatexSuiteConfig } from "src/snippets/codemirror/config";
-import { Context } from "src/utils/context";
 import { tabout } from "src/features/tabout";
+
+
+const ALIGNMENT = " & ";
+const LINE_BREAK = " \\\\\n"
+const LINE_BREAK_INLINE = " \\\\ "
+
+
+const generateSeparatorChange = (separator: string, view: EditorView, range: SelectionRange): { from: number, to: number, insert: string } => {
+	const d = view.state.doc;
+
+	// Insert indents
+	const fromLineText = d.lineAt(range.from).text;
+	const leadingIndents = fromLineText.match(/^\s*/)[0];
+	const fixed_separator = separator.replaceAll("\n", `\n${leadingIndents}`);
+
+	return { from: range.from, to: range.to, insert: fixed_separator };
+}
+
+
+const applySeparator = (separator: string, view: EditorView) => {
+	const sel = view.state.selection;
+	const changes = sel.ranges.map(range => generateSeparatorChange(separator, view, range));
+
+	const tempTransaction = view.state.update({ changes });
+
+	const newSelection = EditorSelection.create(
+		changes.map(({ from, to, insert }) =>
+			EditorSelection.cursor(tempTransaction.changes.mapPos(from) + insert.length)
+		),
+		sel.mainIndex
+	);
+
+	view.dispatch(view.state.update({ changes, selection: newSelection }));
+}
+
 
 export const runMatrixShortcuts = (view: EditorView, ctx: Context, key: string, shiftKey: boolean): boolean => {
 	const settings = getLatexSuiteConfig(view);
@@ -19,41 +55,40 @@ export const runMatrixShortcuts = (view: EditorView, ctx: Context, key: string, 
 
 	if (!isInsideAnEnv) return false;
 
-	// Take main cursor since ctx.mode takes the main cursor, weird behaviour is expected with multicursor because of this.
-	if (key === "Tab" && view.state.selection.main.empty) {
-		view.dispatch(view.state.replaceSelection(" & "));
+	if (key === "Tab") {
+		applySeparator(ALIGNMENT, view);
 
 		return true;
 	}
+
 	else if (key === "Enter") {
-		if (shiftKey && ctx.mode.blockMath) {
-			// Move cursor to end of next line
-			const d = view.state.doc;
+		if (shiftKey) {
+			if (ctx.mode.inlineMath) {
+				tabout(view, ctx);
+			}
+			else {
+				// Move cursor to end of next line
+				const d = view.state.doc;
 
-			const nextLineNo = d.lineAt(ctx.pos).number + 1;
-			const nextLine = d.line(nextLineNo);
+				const nextLineNo = d.lineAt(ctx.pos).number + 1;
+				const nextLine = d.line(nextLineNo);
 
-			setCursor(view, nextLine.to);
-		}
-		else if (shiftKey && ctx.mode.inlineMath) {
-			tabout(view, ctx);
-		}
-		else if (ctx.mode.blockMath) {
-			const d = view.state.doc;
-			const lineText = d.lineAt(ctx.pos).text;
-			const matchIndents = lineText.match(/^\s*/);
-			const leadingIndents = matchIndents ? matchIndents[0] : "";
-
-			view.dispatch(view.state.replaceSelection(` \\\\\n${leadingIndents}`));
+				setCursor(view, nextLine.to);
+			}
 		}
 		else {
-			view.dispatch(view.state.replaceSelection(" \\\\ "));
+			if (ctx.mode.inlineMath) {
+				applySeparator(LINE_BREAK_INLINE, view);
+			}
+			else {
+				applySeparator(LINE_BREAK, view);
+			}
 		}
 
 		return true;
 	}
+
 	else {
 		return false;
 	}
-
 }
