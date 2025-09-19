a1af918bdb2ed07fe7cbea09365127715d1b4aef
superle3 <987654321.yo.na.than@gmail.com>
2025-09-19 21:40:41 +0200

    feat: improve snippets performance v3
    Improve snippet expansion by reducing `view.dispatch` calls.
    Improvement 3:
    Merging placing the snippets down and converting the tabstops syntax to actual snippets
    To convert the syntax, the changes have to applied separately and
    applying the changes to the old tabstops now happens before the new
    tabstops are inserted.
    
    The final 2 `view.dispatch` in `expandSnippets` could be merged by
    altering the undo and redo behaviour and inserting the keys in the
    automatic snippets back in.

--- src/snippets/codemirror/tabstops_state_field.ts
+++ src/snippets/codemirror/tabstops_state_field.ts
@@ -13,6 +13,9 @@ export const tabstopsStateField = StateField.define<TabstopGroup[]>({
 
 	update(value, transaction) {
 		let tabstopGroups = value;
+		// Optimization: tabstops that are added should already have their changes applied
+		// So changes are only applied to existing tabstops
+		tabstopGroups.forEach(grp => grp.map(transaction.changes));
 		for (const effect of transaction.effects) {
 			if (effect.is(addTabstopsEffect)) {
 				tabstopGroups.unshift(...effect.value);
@@ -21,7 +24,6 @@ export const tabstopsStateField = StateField.define<TabstopGroup[]>({
 				tabstopGroups = [];
 			}
 		}
-		tabstopGroups.forEach(grp => grp.map(transaction.changes));
 
 
 		// Remove the tabstop groups that the cursor has passed. This scenario
@@ -77,7 +79,7 @@ export function getTabstopGroupsFromView(view: EditorView) {
 	return currentTabstopGroups;
 }
 
-export function addTabstops(view: EditorView, tabstopGroups: TabstopGroup[]) {
+export function addTabstops(tabstopGroups: TabstopGroup[]) {
 	return ({
 		effects: [addTabstopsEffect.of(tabstopGroups)],
 	});
