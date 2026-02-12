d526e38e4354f012fb7e4cdcc9c4ea4831b3ace1
superle3 <987654321.yo.na.than@gmail.com>
2026-02-12 13:19:34 +0100

    only update version.js when increasing minAppVersion.

--- version-bump.mjs
+++ version-bump.mjs
@@ -2,6 +2,17 @@ import { readFileSync, writeFileSync } from "fs";
 
 const targetVersion = process.env.npm_package_version;
 
+function version_larger_than(v1, v2) {
+	const v1_parts = v1.split(".").map(Number);
+	const v2_parts = v2.split(".").map(Number);
+	for (let i = 0; i < Math.max(v1_parts.length, v2_parts.length); i++) {
+		const v1_part = v1_parts[i] || 0;
+		const v2_part = v2_parts[i] || 0;
+		if (v1_part > v2_part) return true;
+		if (v1_part < v2_part) return false;
+	}
+}
+
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
@@ -10,5 +21,9 @@ writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+const version_keys = Object.keys(versions);
+const highestVersion = version_keys.reduce((a, b) => (version_larger_than(a, b) ? a : b));
+if (versions[highestVersion] !== minAppVersion) {
+	versions[targetVersion] = minAppVersion;
+	writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+}
